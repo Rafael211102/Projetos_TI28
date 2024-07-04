d838bd5b39087ffeb4b1dd33fb4417faf53dc4cf
Rafael211102 <rafaelrodrigo2111@outlook.com>
2024-07-04 11:32:12 -0300

    Adicionando novos files
    Foi feita a criação de novos files (home, usuario-cadastro e usuario-lista)

--- mafia-do-pao/login.php
+++ mafia-do-pao/login.php
@@ -13,9 +13,7 @@ if($_SERVER['REQUEST_METHOD']== 'POST'){
 $retorno = mysqli_query($link, $sql);
 
 
-while($tbl = mysqli_fetch_array($retorno)){
-    $contagem =$tbl[0];
-}
+$contagem = mysqli_fetch_array($retorno) [0];
 
 // VERIFICA SE USUARIO EXISTE
 if($contagem == 1){
@@ -36,18 +34,19 @@ else{
     <link rel="stylesheet" href="css/estilo.css">
     <title>LOGIN USUARIO</title>
 </head>
-<body>
+<body >
+    
     <div class="container-global">
        
         <form class="formulario" action="login.php" method="post">
-        <img src="img/logo.png" width="100px" height= "100px"  alt="">
+        <img src="img/logo.png" width="150px" height= "150px"  alt="">
         <label>LOGIN</label>
         <br>
-        <input type="text" name="txtlogin" required>
+        <input type="text" name="txtlogin" placeholder="Login" required>
         <br>
         <label>SENHA</label>
         <br>
-        <input type="password" name="txtsenha" required>
+        <input type="password" name="txtsenha" placeholder="Senha" required>
         <br>
         <br>
         <input type="submit" value="ACESAR">
---
+++ mafia-do-pao/usuario-cadastro.php
@@ -0,0 +1,61 @@
+<?php 
+include("conectadb.php");
+
+if($_SERVER['REQUEST_METHOD']== 'POST'){
+    $login = $_POST['txtlogin'];
+    $senha =$_POST['txtsenha'];
+    $email =$_POST['txtemail'];
+
+    // VALIDA SE O USUARIO CADASTRAR EXISTE
+    $sql ="SELECT COUNT(usu_id)FROM tb_usuarios WHERE usu_login = '$login' OR usu_email = '$email'";
+
+
+// RETORNO DO BANCO
+$retorno = mysqli_query($link, $sql);
+$contagem = mysqli_fetch_array($retorno) [0];
+
+// VERIFICA SE USUARIO EXISTE
+if($contagem == 0){
+    $sql = "INSERT INTO tb_usuarios(usu_login, usu_senha, usu_email, usu_status) VALUES('$login', '$senha', '$email', '1')";
+    mysqli_query($link, $sql);
+    echo"<script>window.alert('UUSARIO CADSTRADO COM SUCESSO');</script>";
+    echo"<script>window.location.href='lohin.php';</script>";
+}
+else if($contagem >=1){
+    echo"<script>window.alert('USUARIO JÁ EXISTE!');</script>";
+}
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>CADASTRO DE USUARIO</title>
+</head>
+<body >
+    
+    <div class="container-global">
+       <a href="home.php" style="width: 50px; height: 50px; background-color: blue">VOLTAR</a>
+        <form class="formulario" action="usuario-cadastro.php" method="post">
+       
+        <label>LOGIN</label>
+        <br>
+        <input type="text" name="txtlogin" placeholder="Digite seu login" required>
+        <br>
+        <label>SENHA</label>
+        <br>
+        <input type="password" name="txtsenha" placeholder="Digite a sua senha" required>
+        <br>
+        <label>Email</label>
+        <input type="email" name="txtemail" placeholder="Digite o seu email" required>
+        <br>
+        <br>
+        <input type="submit" value="CRIAR">
+</form>
+    </div>
+</body>
+</html>
